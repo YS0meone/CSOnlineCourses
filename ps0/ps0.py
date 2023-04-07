7d72a55e6fa5dc146a49a77a2922f297d5ebdf51
Tim Yuan <timyuan1016@gmai.com>
2023-04-06 18:10:32 -0700

    Adding MI 6.001 labs

---
+++ ps0/ps0.py
@@ -0,0 +1,8 @@
+import math
+# remember to convert input into numerical number
+x = int(input('Please enter a number x\n'))
+y = int(input('Please enter a number y\n'))
+print("x raisd to the power y is {}".format(x**y))
+print("log base 2 of x is {}".format(math.log(x, 2)))
+# type conversions of python
+# str, float, int
